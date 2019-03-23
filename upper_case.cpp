6da3560355209cd3699fdd1213ecb00a6e90bb12
Muhammad Haris Noori <harisnoori20@gmail.com>
2019-03-23 12:30:50 +0500

    Add files via upload

---
+++ upper_case.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void convert(string& s)
+{
+	
+	for(int i = 0; i < s.length(); i++)
+	{
+		s[i] = toupper(s[i]);
+		
+	}
+}
+
+int main()
+{
+	
+	string s;
+	cout << "Enter string:  " << endl;
+	getline(cin , s);
+	convert(s);
+	cout << s << endl;
+	
+	
+	
+}
